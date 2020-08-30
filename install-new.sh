1ae86b70ae236dad49031d258232fff430727713
thaengineer <51303308+thaengineer@users.noreply.github.com>
2020-08-30 03:46:12 -0400

    Update install-new.sh

--- install-new.sh
+++ install-new.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # vars
-DEVICE="sda"
-IFACE="wlan0"
-ESSID="skynet"
+DEVICE=""
+IFACE=""
+ESSID=""
 WLANPASS=''
 HOSTNAME=""
+DOMAIN=""
 ROOTPASS=''
 USER=""
 USERPASS=''
@@ -16,20 +17,20 @@ loadkeys us
 
 # 02 network
 iwctl device list
-iwctl station wlan0 scan
-iwctl station wlan0 get-networks
-iwctl --password ${WLANPASS} station wlan0 connect ${ESSID} psk
+iwctl station ${IFACE} scan
+iwctl station ${IFACE} get-networks
+iwctl --password ${WLANPASS} station ${IFACE} connect ${ESSID} psk
 
 # 03 time
 timedatectl set-ntp true
 
 # 04 partitions
 parted /dev/${DEVICE} mklabel gpt
-parted /dev/${DEVICE} mkpart primary ext4 0% 100%
+parted /dev/${DEVICE} mkpart primary btrfs 0% 100%
 parted /dev/${DEVICE} set 1 boot on
 
 # 05 filesystem
-mkfs.ext -F /dev/${DEVICE}1
+mkfs.btrfs /dev/${DEVICE}1
 mount /dev/${DEVICE}1 /mnt
 
 # 06 mirrors
@@ -43,23 +44,36 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # 08 chroot
 arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime"
-arch-chroot /mnt /bin/bash -c "hwclock --systohc"
+arch-chroot /mnt /bin/bash -c "hwclock --systohc --utc"
 arch-chroot /mnt /bin/bash -c "echo \"en_US.UTF-8 UTF-8\" > /etc/locale.gen"
 arch-chroot /mnt /bin/bash -c "locale-gen"
 arch-chroot /mnt /bin/bash -c "echo \"LANG=en_US.UTF-8\" > /etc/locale.conf"
 arch-chroot /mnt /bin/bash -c "echo \"KEYMAP=us\" > /etc/vconsole.conf"
-arch-chroot /mnt /bin/bash -c "echo \"anubis\" > /etc/hostname"
-arch-chroot /mnt /bin/bash -c "echo \"127.0.0.1      localhost\" > /etc/hosts"
-arch-chroot /mnt /bin/bash -c "echo \"::1            localhost\" >> /etc/hosts"
-arch-chroot /mnt /bin/bash -c "echo \"127.0.0.1      anubis.localdomain anubis\" >> /etc/hosts"
-arch-chroot /mnt /bin/bash -c "mkinitcpio -P"
-arch-chroot /mnt /bin/bash -c "echo -e \"07031991\n07031991\n\" | passwd"
-arch-chroot /mnt /bin/bash -c "useradd -G network,power,users -m -s /usr/bin/bash -U stephen"
+arch-chroot /mnt /bin/bash -c "echo \"${HOSTNAME}\" > /etc/hostname"
+arch-chroot /mnt /bin/bash -c "echo \"# IPv4\" > /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"127.0.0.1      localhost\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"127.0.1.1      ${HOSTNAME}.${DOMAIN} ${HOSTNAME}\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"# IPv6\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"::1            localhost ip6-localhost ip6-loopback\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"ff02::1         ip6-allnodes\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"ff02::2         ip6-allrouters\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "touch /etc/resolv.conf"
+
+arch-chroot /mnt /bin/bash -c "mkinitcpio -p linux"
+arch-chroot /mnt /bin/bash -c "echo -e \"${ROOTPASS}\n${ROOTPASS}\n\" | passwd"
+arch-chroot /mnt /bin/bash -c "useradd -G users,wheel -m -s /usr/bin/bash -U ${USER}"
+arch-chroot /mnt /bin/bash -c "echo -e \"${USERPASS}\n${USERPASS}\n\" | passwd ${USER}"
 arch-chroot /mnt /bin/bash -c "pacman -S grub os-prober --noconfirm"
-arch-chroot /mnt /bin/bash -c "grub-install --recheck --target=i386-pc /dev/sda"
+arch-chroot /mnt /bin/bash -c "grub-install --recheck --target=i386-pc /dev/${DEVICE}"
 arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
 
+#arch-chroot /mnt /bin/bash -c "echo \"Section "" \" >> /etc/X11/xorg.conf.d/10-keyboard.conf"
+
 arch-chroot /mnt /bin/bash -c "# pacman -S <pkgs> --noconfirm"
+
+alsamixer
+alsactl store
 
 #  09 unmount
 umount -R /mnt
@@ -69,3 +83,12 @@ clear
 echo "Installation complete. The system will reboot in 30 seconds."
 sleep 30
 systemctl reboot
+
+# ----------------------------------------
+# ----------------------------------------
+
+# post install
+pacman -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+netctl disable ${IFACE}-*
