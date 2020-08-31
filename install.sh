912673c87f25e36682e56453d76363db496b8475
thaengineer <51303308+thaengineer@users.noreply.github.com>
2020-08-30 23:03:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,8 @@ sleep 5
 timedatectl set-ntp true
 
 # 04 partitions
-parted /dev/${DEVICE} mklabel gpt
+# parted /dev/${DEVICE} mklabel gpt
+parted /dev/${DEVICE} mklabel msdos
 parted /dev/${DEVICE} mkpart primary btrfs 0% 100%
 parted /dev/${DEVICE} set 1 boot on
 
