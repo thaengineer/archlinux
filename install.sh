f3368b4d9532b62037c246f383448c74cd786883
thaengineer <51303308+thaengineer@users.noreply.github.com>
2020-08-30 21:10:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,136 +1,95 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # vars
-DEVICE="sda"
-PARTSTART=""
-PARTEND=""
-SWAPSTART="$(expr ${PARTEND} - 8)"
-WNIC="$(ip link | grep 'wl' | awk '{ print $2 }' | sed 's/\://')"
-NAMESRV1="1.1.1.1"
-NAMESRV2="1.0.0.1"
-HOSTNAME=""
-ROOTPW=""
-USERNAME=""
-USERPW=""
+DEVICE=""
+IFACE=""
 ESSID=""
-WIFIKEY=""
+WLANPASS=''
+HOSTNAME=""
+DOMAIN=""
+ROOTPASS=''
+USERNAME=""
+USERPASS=''
 
 
-# 01 partitioning
-parted /dev/${DEVICE} mklabel msdos
-parted /dev/${DEVICE} mkpart primary ext4 $(($PARTSTART))MiB $(($SWAPSTART))GiB
+# 01 keyboard layout
+loadkeys us
+
+# 02 connect to network
+iwctl device list
+iwctl station ${IFACE} scan
+iwctl station ${IFACE} get-networks
+iwctl --password ${WLANPASS} station ${IFACE} connect ${ESSID} psk
+
+# 03 time
+timedatectl set-ntp true
+
+# 04 partitions
+parted /dev/${DEVICE} mklabel gpt
+parted /dev/${DEVICE} mkpart primary btrfs 0% 100%
 parted /dev/${DEVICE} set 1 boot on
-parted /dev/${DEVICE} mkpart primary linux-swap $(($SWAPSTART))GiB $(($PARTEND))GiB
 
-
-# 02 format partitions
-mkfs.ext4 -F /dev/${DEVICE}1
-mkswap -c -f /dev/${DEVICE}2
-swapon -d /dev/${DEVICE}2
-
-
-# 03 mount partitions
+# 05 filesystem
+mkfs.btrfs -L root -n 64k /dev/${DEVICE}1
 mount /dev/${DEVICE}1 /mnt
 
-
-# 04 connect to network
-echo -e "# ${ESSID}\nInterface=${WNIC}\nConnection=wireless\nSecurity=wpa\nESSID=${ESSID}\nIP=dhcp\nKey=${WIFIKEY}" > /etc/netctl/${ESSID}
-chmod 600 /etc/netctl/${ESSID}
-netctl start ${ESSID}
-
-
-# 05 select pacman mirrors
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+# 06 mirrors
 sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
 sed -i '95s/^#Include/Include/' /etc/pacman.conf
 
-
-# 06 install base
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
-sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf
-sed -i '94s/^#Include/Include/' /etc/pacman.conf
+# 07 install base
 pacman -Sy --noconfirm
-pacstrap -i /mnt base base-devel bash-completion bzip2 cronie cups curl dhcpcd dialog gdb git gnupg grub gvfs-mtp gzip htop ifplugd iftop iproute2 iptables iw mesa mtpfs netcat ntfs-3g openssh openvpn os-prober p7zip parted ppp rsync rxvt-unicode screen sudo tar tlp tmux ufw unrar unzip vim wget wireless_tools wpa_supplicant xz zip --noconfirm
-genfstab -U -p /mnt > /mnt/etc/fstab
-sed -i '5s/data=ordered/discard/' /mnt/etc/fstab
-
+pacstrap -i /mnt base base-devel linux linux-firmware --noconfirm
+genfstab -U -p /mnt >> /mnt/etc/fstab
+# sed -i '5s/data=ordered/discard/' /mnt/etc/fstab
 
 # 08 configure network
-echo -e "# ${ESSID}\nInterface=${WNIC}\nConnection=wireless\nSecurity=wpa\nESSID=${ESSID}\nIP=dhcp\nKey=${WIFIKEY}" > /etc/netctl/${ESSID}
+echo -e "# ${ESSID}\nInterface=${IFACE}\nConnection=wireless\nSecurity=wpa\nESSID=${ESSID}\nIP=dhcp\nKey=${WLANPASS}" > /etc/netctl/${ESSID}
 chmod 600 /mnt/etc/netctl/${ESSID}
 
-
-# 09 locale/lang
-echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-arch-chroot /mnt /bin/bash -c "locale-gen"
-
-
-# 10 pacman mirrors
-arch-chroot /mnt /bin/bash -c "sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist"
+# 09 chroot
+arch-chroot /mnt /bin/bash -c "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg PKGBUILD && pacman -U yay*.zst --noconfirm"
+arch-chroot /mnt /bin/bash -c "yay -Sy --noconfirm"
+arch-chroot /mnt /bin/bash -c "yay -S aircrack-ng alsa-utils anydesk bash-completion bleachbit blender bless bzip2 compton cronie cups curl dhcpcd dialog docker dsniff file-roller firefox gdb gimp git gnupg gparted grc grub gvfs-mtp gzip hashcat htop ifplugd iftop iproute2 iptables irssi iw jre8-openjdk keepassxc krita libreoffice-fresh mesa mtpfs netcat nitrogen nmap noto-fonts ntfs-3g ntfsfixboot nvidia nvidia-utils lib32-nvidia-utils openbox openssh openvpn os-prober p7zip parted pcmanfm pidgin polybar popcorntime qemu radare2 radare2-cutter redshift rsync rxvt-unicode screen shotwell slim sublime-text-dev sudo tar tcpdump terminus-font tlp tmux tor tor-browser torsocks transmission-gtk ttf-dejavu ttf-droid ttf-fira-mono ttf-font-awesome ttf-inconsolata ufw unrar unzip vim virtualbox virtualbox-host-modules-arch vlc wget wireless_tools wireshark-gtk wpa_supplicant xf86-input-libinput xorg-apps xorg-server xz zip --noconfirm"
+arch-chroot /mnt /bin/bash -c "ln -sf /opt/sublime_text_3/sublime_text /usr/bin/subl"
 arch-chroot /mnt /bin/bash -c "sed -i 's/^#\[multilib\]/\[multilib\]/' /etc/pacman.conf"
 arch-chroot /mnt /bin/bash -c "sed -i '94s/^#Include/Include/' /etc/pacman.conf"
-
-
-# 11 time
-arch-chroot /mnt /bin/bash -c "ln -f -s /usr/share/zoneinfo/America/New_York /etc/localtime"
+arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime"
 arch-chroot /mnt /bin/bash -c "hwclock --systohc --utc"
 arch-chroot /mnt /bin/bash -c "timedatectl set-timezone America/New_York"
 arch-chroot /mnt /bin/bash -c "timedatectl set-ntp true"
-
-
-# 12 dns
-echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1" > /mnt/etc/resolvconf.conf
-
-
-# 13 bootloader
-arch-chroot /mnt /bin/bash -c "pacman -S grub os-prober --noconfirm"
-arch-chroot /mnt /bin/bash -c "grub-install --recheck --target=i386-pc /dev/sda"
-arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
+arch-chroot /mnt /bin/bash -c "echo \"en_US.UTF-8 UTF-8\" > /etc/locale.gen"
+arch-chroot /mnt /bin/bash -c "locale-gen"
+arch-chroot /mnt /bin/bash -c "echo \"LANG=en_US.UTF-8\" > /etc/locale.conf"
+arch-chroot /mnt /bin/bash -c "echo \"KEYMAP=us\" > /etc/vconsole.conf"
+arch-chroot /mnt /bin/bash -c "echo \"${HOSTNAME}\" > /etc/hostname"
+arch-chroot /mnt /bin/bash -c "echo \"# IPv4\" > /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"127.0.0.1      localhost\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"127.0.1.1      ${HOSTNAME}.${DOMAIN} ${HOSTNAME}\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"# IPv6\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"::1            localhost ip6-localhost ip6-loopback\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"ff02::1         ip6-allnodes\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "echo \"ff02::2         ip6-allrouters\" >> /etc/hosts"
+arch-chroot /mnt /bin/bash -c "touch /etc/resolv.conf"
 arch-chroot /mnt /bin/bash -c "mkinitcpio -p linux"
-
-
-# 14 hostname
-echo "anubis" > /mnt/etc/hostname
-
-
-# 15 root
-arch-chroot /mnt /bin/bash -c "echo -e \"${ROOTPW}\n${ROOTPW}\" | passwd"
-
-
-# 16 sudoers
-arch-chroot /mnt /bin/bash -c "sed -i '82s/^# //' /etc/sudoers"
-
-
-# 18 create user
-arch-chroot /mnt /bin/bash -c "useradd -m -G audio,video,storage,optical,network,users,wheel,rfkill,scanner,power -s /bin/bash ${USERNAME}"
-arch-chroot /mnt /bin/bash -c "echo -e \"${USERPW}\n${USERPW}\" | passwd ${USERNAME}"
-arch-chroot /mnt /bin/bash -c "mkdir Desktop Documents Downloads Music Pictures Videos"
-arch-chroot /mnt /bin/bash -c "cd /home/${USERNAME}; mkdir Desktop Documents Downloads Music Pictures Videos workspace"
-arch-chroot /mnt /bin/bash -c "git clone https://gitlab.com/thaengineer/dotfiles"
-arch-chroot /mnt /bin/bash -c "mv ~/dots/.bash_profile /home/${USERNAME}"
-arch-chroot /mnt /bin/bash -c "mv ~/dots/.bashrc /home/${USERNAME}"
-arch-chroot /mnt /bin/bash -c "mv ~/dots/.vimrc /home/${USERNAME}"
-arch-chroot /mnt /bin/bash -c "mv ~/dots/.xinitrc /home/${USERNAME}"
-arch-chroot /mnt /bin/bash -c "mv ~/dots/.Xresources /home/${USERNAME}"
-arch-chroot /mnt /bin/bash -c "mv -r ~/dots/.config /home/${USERNAME}/.config"
-arch-chroot /mnt /bin/bash -c "mv -r ~/dots/.git /home/${USERNAME}/.git"
-arch-chroot /mnt /bin/bash -c "rm -rf ~/dots"
-arch-chroot /mnt /bin/bash -c "chmod -R 755 /home/${USERNAME}/*"
-arch-chroot /mnt /bin/bash -c "chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/*"
+arch-chroot /mnt /bin/bash -c "echo -e \"${ROOTPASS}\n${ROOTPASS}\n\" | passwd"
+arch-chroot /mnt /bin/bash -c "useradd -G users,wheel -m -s /usr/bin/bash -U ${USERNAME}"
+arch-chroot /mnt /bin/bash -c "echo -e \"${USERPASS}\n${USERPASS}\n\" | passwd ${USERNAME}"
+arch-chroot /mnt /bin/bash -c "grub-install --recheck --target=i386-pc /dev/${DEVICE}"
+arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
 arch-chroot /mnt /bin/bash -c "sed -i '69s/^#default_user       simone/default_user       ${USERNAME}/' /etc/slim.conf"
 arch-chroot /mnt /bin/bash -c "sed -i '77s/^#auto_login          no/^#auto_login          yes/' /etc/slim.conf"
 arch-chroot /mnt /bin/bash -c "systemctl enable slim"
-mv postinstall.sh /mnt/home/${USERNAME}
-chown stephen:stephen /mnt/home/${USERNAME}/postinstall.sh
-chmod 755 /mnt/home/${USERNAME}/postinstall.sh
+arch-chroot /mnt /bin/bash -c "alsactl store"
+arch-chroot /mnt /bin/bash -c "netctl disable dhcpcd"
+arch-chroot /mnt /bin/bash -c "netctl enable ${ESSID}"
 
-
-# 19 unmount partitions
+# 10 unmount
 umount -R /mnt
 
-
-# 20 reboot system
-echo "The installation is complete. The system will reboot in 10 seconds."
-sleep 10
+# 11 restart
+clear
+echo "Installation complete. The system will reboot in 30 seconds."
+sleep 30
 systemctl reboot
