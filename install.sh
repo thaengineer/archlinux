9584f113890799d8d7958180d842b960cc441051
Stephen <51303308+thaengineer@users.noreply.github.com>
2022-03-02 02:36:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,12 @@ timedatectl set-ntp true
 
 # 04 partitions
 parted /dev/${DEVICE} mklabel gpt
-parted /dev/${DEVICE} mkpart primary btrfs 0% 100%
+parted /dev/${DEVICE} mkpart primary ext4 0% 100%
 parted /dev/${DEVICE} set 1 boot on
 
 # 05 filesystem
-# mkfs.ext4 -F /dev/${DEVICE}1
-mkfs.btrfs -f /dev/${DEVICE}1
+mkfs.ext4 -F /dev/${DEVICE}1
+# mkfs.btrfs -f /dev/${DEVICE}1
 mount /dev/${DEVICE}1 /mnt
 
 # 06 mirrors
