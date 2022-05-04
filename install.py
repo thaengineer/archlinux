3cb4b8212aed0db1bd4e7f06ab95fc893d3b724c
Stephen <51303308+thaengineer@users.noreply.github.com>
2022-05-04 08:05:33 -0400

    Update install.py

--- install.py
+++ install.py
@@ -2,9 +2,8 @@
 import os
 import sys
 from os import listdir, path
-import subprocess as bash
+import subprocess as s
 
-# vars
 username = ''
 packages = [
     'base',
@@ -14,17 +13,18 @@ packages = [
     ''
 ]
 
+def install():
+    # foo bar
 
-# functions
-bash.check_output('ls -ltr', shell=True).decode().strip('\n')
-bash.call(f'ls -ltr {var}', shell=True)
+s.check_output('ls -ltr', shell=True).decode().strip('\n')
+s.call(f'ls -ltr {var}', shell=True)
 
 
 def main():
     try:
-        if sys.argv[1] == '-i' or sys.argv[1] == '--install':
+        if '-i' in sys.argv[1]:
             install()
-        elif sys.argv[1] == '-p' or sys.argv[1] == '--post-install':
+        elif '-p' in sys.argv[1]:
             post_install()
         else:
             print(f'{help}')
@@ -33,6 +33,5 @@ def main():
     print('Installing Arch Linux...')
 
 
-# run main function
 if __name__ == '__main__':
     main()
